53c6bb90a44ee0949f856e2aee00b59dca96bd98
hooria178 <72269201+hooria178@users.noreply.github.com>
2022-11-11 22:13:19 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "disk.h"
 #include "fs.h"
@@ -12,162 +13,199 @@
 #define FAT_FREE 0
 
 #define disk_error(fmt, ...) \
-	fprintf(stderr, "%s: "fmt"\n", __func__, ##__VA_ARGS__)
+	fprintf(stderr, "%s: " fmt "\n", __func__, ##__VA_ARGS__)
 
 /* TODO: Phase 1 */
 
 struct __attribute__((__packed__)) superblock
 {
-	char signature[8]; //(8 characters) signature
+	char signature[8];			  //(8 characters) signature
 	uint16_t numBlockVirtualDisk; //(2 bytes) total amount of blocks of virtual disk
-	uint16_t rootBlockIndex; //(2 bytes) root directory block index
+	uint16_t rootBlockIndex;	  //(2 bytes) root directory block index
 	uint16_t dataBlockStartIndex; //(2 bytes) data block start index
-	uint16_t numDataBlocks;// (2 bytes)  amount of data blcoks
-	uint8_t numBlocksFAT; //(1 bytes)// number of blocks for FAT
-	uint8_t padding[4079]; //(4079 bytes)// unsused/padding  ASK THIS 
-	
+	uint16_t numDataBlocks;		  // (2 bytes)  amount of data blcoks
+	uint8_t numBlocksFAT;		  //(1 bytes)// number of blocks for FAT
+	uint8_t padding[4079];		  //(4079 bytes)// unsused/padding  ASK THIS
 };
-
 
 struct __attribute__((__packed__)) FAT
 {
 	uint16_t next;
-	
 };
 
 struct __attribute__((__packed__)) rootdirectory
 {
-	
+
 	char fileName[FS_FILENAME_LEN]; //(16 bytes) Filename
-	uint32_t sizeOfFile; //(4 bytes) Size of the files (in bytes)
-	uint16_t firstIndex; //(2 bytes) Index of the first data block
-	uint8_t padding[10]; //(10 bytes) Unused/Padding
-	
+	uint32_t sizeOfFile;			//(4 bytes) Size of the files (in bytes)
+	uint16_t firstIndex;			//(2 bytes) Index of the first data block
+	uint8_t padding[10];			//(10 bytes) Unused/Padding
 };
 
-//creating objects of structs
+// creating objects of structs
 struct superblock *superBlock;
 struct FAT *fatArray;
 struct rootdirectory *rootDirectory;
-/**
- * fs_mount - Mount a file system
- * @diskname: Name of the virtual disk file
- *
- * Open the virtual disk file @diskname and mount the file system that it
- * contains. A file system needs to be mounted before files can be read from it
- * with fs_read() or written to it with fs_write().
- *
- * Return: -1 if virtual disk file @diskname cannot be opened, or if no valid
- * file system can be located. 0 otherwise.
- */
+bool disk_open = false;
 
 int fs_mount(const char *diskname)
 {
-	/*
-	fs_mount() makes the file system contained 
-	in the specified virtual disk “ready to be used”.
-	You need to open the virtual disk, using the 
-	block API, and load the meta-information that is
-	necessary to handle the file system operations
-	described in the following phases. 
-
-
-	Don’t forget that your function fs_mount() 
-	should perform some error checking in order 
-	to verify that the file system has the expected 
-	format. For example, the signature of the file
-	system should correspond to the one defined by
-	the specifications, the total amount of block
-	should correspond to what block_disk_count()
-	returns, etc.
-	*/
-
-	//open disk
-	//block_disk_open(const char *diskname);
-	//if virtual file disk cannot be openned return -1;
+	// open disk
+	// if virtual file disk cannot be openned return -1;
 	if (block_disk_open(diskname) == -1)
 	{
 		disk_error("disk cannot be opened");
 		return -1;
 	}
+	disk_open = true;
 
-	//declaring a super block
-	superBlock = malloc(sizeof(BLOCK_SIZE));
-	if (block_read(0,(void *)superBlock) == -1)
+	// declaring a super block
+	superBlock = malloc(BLOCK_SIZE);
+	if (superBlock == NULL)
+	{
+		perror("Mem superblock fail");
+		return -1;
+	}
+
+	if (block_read(0, (void *)superBlock) == -1)
 	{
 		disk_error("Cannot read from block");
 		return -1;
 	}
 
-	//checking signature
-	if (memcmp(superBlock->signature, "ECS150FS", 8) != 0){
+	// checking signature
+	if (strncmp(superBlock->signature, "ECS150FS", 8) != 0)
+	{
 		disk_error("Signature not matched");
 		return -1;
 	}
 
-	//block's disk count check
-	if (block_disk_count() != superBlock->numBlockVirtualDisk){
+	if (block_disk_count() != superBlock->numBlockVirtualDisk)
+	{
 		disk_error("Number of blocks not matched");
 		return -1;
 	}
-	
-	//fatArray initialization
-	fatArray = malloc(sizeof(superBlock->numBlocksFAT * BLOCK_SIZE));
+
+	// fatArray initialization
+	fatArray = malloc(superBlock->numBlocksFAT * BLOCK_SIZE);
 	for (int i = 0; i < superBlock->numBlocksFAT; i++)
 	{
-		if (block_read(i + 1, (void *)fatArray + (i*BLOCK_SIZE))== -1)
+		if (block_read(i + 1, (void *)fatArray + (i * BLOCK_SIZE)) == -1)
 		{
 			disk_error("Error in reading");
 			return -1;
 		}
-
 	}
 
-	//rootDirectory initialization
-	rootDirectory = malloc(sizeof(struct rootdirectory)* FS_FILE_MAX_COUNT);
-	if (block_read(superBlock->numBlocksFAT+1,(void *)rootDirectory) == -1)
+	// rootDirectory initialization
+	rootDirectory = malloc(sizeof(struct rootdirectory) * FS_FILE_MAX_COUNT);
+	if (block_read(superBlock->numBlocksFAT + 1, (void *)rootDirectory) == -1)
 	{
 		disk_error("Cannot read from root directory block");
 		return -1;
 	}
-	
+
 	return 0;
 }
 
+/**
+ * fs_umount - Unmount file system
+ *
+ * Unmount the currently mounted file system and close the underlying virtual
+ * disk file.
+ *
+ * Return: -1 if no FS is currently mounted, or if the virtual disk cannot be
+ * closed, or if there are still open file descriptors. 0 otherwise.
+ */
+/*
+	This means that whenever fs_umount() is called, all meta-information
+	and file data must have been written out to disk.
+*/
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
+	/* If no disk is opened, return -1 */
+	if (disk_open == false)
+	{
+		disk_error("No disk opened");
+		return -1;
+	}
+	/*Error: No FS is currently mounted */
+	if (!superBlock)
+	{
+		disk_error("No FS mounted");
+		return -1;
+	}
+
+	// Writing metadata to the disk
+	if (block_write(0, (void *)superBlock) == -1)
+	{
+		disk_error("Error in writing metadata to disk");
+		return -1;
+	}
+
+	// Writing file data to disk
+
+	for (int i = 0; i < superBlock->numBlocksFAT; i++)
+	{
+		if (block_write(i + 1, (void *)fatArray + (i * BLOCK_SIZE)) == -1)
+		{
+			disk_error("Error in writing FAT blocks to disk");
+			return -1;
+		}
+	}
+
+	if (block_write(superBlock->numBlocksFAT + 1, (void *)rootDirectory) == -1)
+	{
+		disk_error("Error in writing root directory blocks to disk");
+		return -1;
+	}
+
+	/*Error: There are file descriptors still open: STILL NEED TO IMPLEMENT*/
+
+	free(superBlock);
+	free(fatArray);
+	free(rootDirectory);
+
+	/*Error: Virtual disk can not be closed */
+	if (block_disk_close() < 0)
+	{
+		disk_error("Virtual disk can not be closed");
+		return -1;
+	}
+
 	return 0;
 }
 
 int fs_info(void)
 {
-	//Count the number of free spaces in FAT array
+	// Count the number of free spaces in FAT array
 	int fatFreeSpaceCount = 0;
 	for (int i = 0; i < superBlock->numDataBlocks; i++)
 	{
-		if(fatArray[i].next == FAT_FREE)
+		if (fatArray[i].next == FAT_FREE)
 		{
 			fatFreeSpaceCount++;
 		}
 	}
 
-	//count the number of free root directories
+	// count the number of free root directories
 	int freeRootDirectoryCount = 0;
-	for ( int  i=0; i< FS_FILE_MAX_COUNT; i++){
-		if ( rootDirectory[i].sizeOfFile == 0){
+	for (int i = 0; i < FS_FILE_MAX_COUNT; i++)
+	{
+		if (rootDirectory[i].sizeOfFile == 0)
+		{
 			freeRootDirectoryCount++;
 		}
 	}
 
-	printf("FS Info:");
-	printf("total_blk_count: %d\n", superBlock->numBlockVirtualDisk );
-	printf("fat_blk_count: %d\n", superBlock->numBlocksFAT );
-	printf("rdir_blk: %d\n", superBlock->numBlocksFAT+1);
-	printf("data_blk: %d\n", superBlock->numBlocksFAT+2 );
-	printf("data_blk_count: %d\n", superBlock->numDataBlocks);
-	printf("fat_free_ratio: %d/%d\n", fatFreeSpaceCount, superBlock->numDataBlocks);
-	printf("rdir_free_ratio: %d/%d\n", freeRootDirectoryCount, FS_FILE_MAX_COUNT);
+	printf("FS Info:\n");
+	printf("total_blk_count=%d\n", superBlock->numBlockVirtualDisk );
+	printf("fat_blk_count=%d\n", superBlock->numBlocksFAT );
+	printf("rdir_blk=%d\n", superBlock->numBlocksFAT+1);
+	printf("data_blk=%d\n", superBlock->numBlocksFAT+2 );
+	printf("data_blk_count=%d\n", superBlock->numDataBlocks);
+	printf("fat_free_ratio=%d/%d\n", fatFreeSpaceCount, superBlock->numDataBlocks);
+	printf("rdir_free_ratio=%d/%d\n", freeRootDirectoryCount, FS_FILE_MAX_COUNT);
 	return 0;
 }
 
